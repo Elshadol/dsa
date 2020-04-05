fc7335f0bae9de44a4ecc779ee7e700bbf5bdc4b
Elshadol <1623200041@qq.com>
2020-04-05 20:49:51 +0800

    a premature jumpsearch implementation

--- search/jumpsearch.c
+++ search/jumpsearch.c
@@ -1,5 +1,22 @@
-#include <stdbool.h>
+#include <math.h>
 
-bool jumpsearch(int a[], int lo, int hi, int t)
+int jumpsearch(int a[], int N, int key)
 {
+	int step = (int)sqrt((double)N);
+	int start = 0;
+	int end = step;
+
+	while ((end < N) && (a[end] <= key)) {
+		start = end;
+		end += step;
+		if (end >= N)
+			end = N;
+	}
+
+	// now a[start] <= key < a[end]
+	while (end > start) {
+		if (a[--end] == key)
+			return end;
+	}
+	return -1;
 }
