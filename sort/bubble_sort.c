c32ff523a9d6431e2dc28740ee0c74d0e47fb4fd
Elshadol <1623200041@qq.com>
2020-04-06 08:55:22 +0800

    some bubble_sort implementations

--- sort/bubble_sort.c
+++ sort/bubble_sort.c
@@ -1,10 +1,49 @@
-void bubble_sort1(int a[], int n)
+#include <stdio.h>
+#include <stdbool.h>
+
+static inline void _swap(int *a, int *b)
 {
-	for (int i = 0; i < n; ++i) {
-		for (int j = 1; j < n - i; ++j) {
-			if (a[j - 1] < a[j]) {
-				swap(a[j - 1], a[j]);
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+
+void bubble_sort0(int a[], int lo, int hi)
+{
+	for (int i = hi; lo < i; --i) {
+		for (int j = lo + 1; j < hi; ++j) {
+			if (a[j - 1] > a[j])
+				_swap(&a[j - 1], &a[j]);
+		}
+	}
+}
+
+void bubble_sort1(int a[], int lo, int hi)
+{
+	bool sorted = false;
+	while (!sorted) {
+		sorted = true;
+		for (int i = lo; ++i < hi; ) {
+			if (a[i - 1] > a[i]) {
+				_swap(&a[i - 1], &a[i]);
+				sorted = false;
 			}
 		}
+		--hi;
+	}
+}
+
+void bubble_sort2(int a[], int lo, int hi)
+{
+	while (lo < hi) {
+		int last = lo;
+		for (int i = lo; ++i < hi; ) {
+			if (a[i - 1] < a[i]) {
+				_swap(&a[i - 1], &a[i]);
+				last = i;
+			}
+		}
+		hi = last;
 	}
 }
