42a0035cc9c1b1d2d9b09b9e5c87d78a671a0efa
Elshadol <1623200041@qq.com>
2020-04-25 20:53:47 +0800

    fix some promblems

--- sort/shell_sort.c
+++ sort/shell_sort.c
@@ -1,3 +1,5 @@
+#include "sort.h"
+
 void shell_sort(int a[], int lo, int hi)
 {
 	int h = 1;
@@ -9,10 +11,37 @@ void shell_sort(int a[], int lo, int hi)
 		for (int i = lo + h; i < hi; i++) {
 			int pivot = a[i];
 			int j = i;
-			for (; (lo <= j - h) && (pivot < a[j - h]); j -= h)
+			for (; (lo + h <= j) && (pivot < a[j - h]); j -= h)
 				a[j] = a[j - h];
 
 			a[j] = pivot;
+		}
+	}
+}
+void shell_sort(int a[], int lo, int hi)
+{
+	int h = 1;
+	while (h < (hi - lo)/3)
+		h = h*3 + 1;
+
+	for (; h > 0; h /= 3) {
+		for (int i = 0; i < h; ++i) {
+			for (int j = lo + i + h; j < hi; j += h) {
+				int pivot = a[i];
+				int k = j;
+				if (pivot < a[lo + i]) {
+					while (lo + i + h <= k) {
+						a[k] = a[k - h];
+						k -= h;
+					}
+				} else {
+					while (pivot < a[k - h]) {
+						a[k] = a[k - h];
+						k -= h;
+					}
+				}
+				a[k] = pivot;
+			}
 		}
 	}
 }
