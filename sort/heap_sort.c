2c9d7643cf05f44b794ee4d7f86f3fd8b8a37397
Elshadol <1623200041@qq.com>
2020-06-06 21:57:52 +0800

    change the implementaiont of heapsort

--- sort/heap_sort.c
+++ sort/heap_sort.c
@@ -1,92 +1,48 @@
 #include "sort.h"
 
-#define PARENT_OF(i) (((i) - 1) / 2)
-#define RIGHT_CHILD_OF(p) ((p) * 2 + 2)
-
-static void _percolate_down(int a[], int N, int p)
+static inline void _sift_down(int a[], int top, int last)
 {
-	int temp = a[p];
-	int c = RIGHT_CHILD_OF(p);
+    int tmp = a[top];
+    int max_idx = top;
+    while (last >= (max_idx += max_idx)) {
+        if ((max_idx != last) && (a[max_idx] < a[max_idx + 1]))
+            ++max_idx;
 
-	for (; c < N; p = c, c = RIGHT_CHILD_OF(p)) {
-		if (a[c] < a[c - 1])
-			--c;
+        if (tmp >= a[max_idx])
+            break;
 
-		if (a[c] < temp)
-			break;
-
-		a[p] = a[c];
-	}
-
-	if (c == N) {
-		--c;
-		if (temp < a[c]) {
-			a[p] = a[c];
-			p = c;
-		}
-	}
-
-	a[p] = temp;
+        a[top] = a[max_idx];
+        top = max_idx;
+    }
 }
 
-static inline void _heapificate(int a[], int N)
+static inline void _sift_down0(int a[], int last)
 {
-	for (int i = PARENT_OF(N - 1); 0 <= i; --i) {
-		_percolate_down(a, N, i);
-	}
+    int tmp = a[0];
+    if (a[0] < a[1]) {
+        tmp = a[1];
+        a[1] = a[0];
+        _sift_down(a, 1, last);
+    }
+    a[0] = a[last];
+    a[last] = tmp;
 }
 
-static inline int _adjust_after_pop_heap(int a[], int N)
+static inline void _heap_sort(int a[], int n)
 {
-	int p = 0;
-	int c = RIGHT_CHILD_OF(p);
+    int i = n >> 1;
+    --n;
+    for ( ; 0 < i; --i)
+        _sift_down(a, i, n);
 
-	while (c < N) {
-		if (a[c] < a[c - 1])
-			--c;
-
-		a[p] = a[c];
-		p = c;
-		c = RIGHT_CHILD_OF(p);
-	}
-
-	if (c == N) {
-		a[p] = a[c - 1];
-		p = c - 1;
-	}
-
-	return p;
-}
-
-static inline void _push_heap(int a[], int last, int val)
-{
-	int i = last;
-
-	while ((0 < i) && (a[PARENT_OF(i)] < val)) {
-		a[i] = a[PARENT_OF(i)];
-		i = PARENT_OF(i);
-	}
-
-	a[i]  = val;
+    for (i = n; 0 <= i; --i)
+        _sift_down0(a, i);
 }
 
 void heap_sort(int a[], int lo, int hi)
 {
-	if (hi - lo < 2)
-		return;
+    if (hi - lo < 2)
+        return;
 
-	int *b = a + lo;
-	const int N = hi - lo;
-
-	_heapificate(b, N);
-
-	for (int i = N; 0 < i; --i) {
-		int temp = b[0];
-
-		int last = _adjust_after_pop_heap(b, i);
-		int right = b[i - 1];
-		_push_heap(b, last, right);
-
-		b[i - 1] = temp;
-	}
+    _heap_sort(a + lo, hi - lo);
 }
