983088c20cd4a32038545e7c228b29081f8a117f
Elshadol <1623200041@qq.com>
2020-06-15 20:36:08 +0800

    fix some bugs

--- bbst/rb/rbtree.h
+++ bbst/rb/rbtree.h
@@ -28,28 +28,28 @@ struct rb_root {
     (type *)( (char *)__mptr - offsetof(type,member) );})
 #endif
 
-#define RB_ROOT (struct rb_root) { NULL, }
 #define rb_entry(ptr, type, member) container_of(ptr, type, member)
 
-void rb_insert_color(struct rb_node *, struct rb_root *);
-void rb_erase(struct rb_node *, struct rb_root *);
+#define RB_ROOT (struct rb_root) { NULL, }
 
-struct rb_node *rb_next(struct rb_node *);
-struct rb_node *rb_prev(struct rb_node *);
-struct rb_node *rb_first(struct rb_node *);
-struct rb_node *rb_last(struct rb_node *);
+void rb_insert_color(struct rb_node *newnode, struct rb_root *root);
+void rb_erase(struct rb_node *node, struct rb_root *root);
+
+struct rb_node *rb_next(struct rb_node *node);
+struct rb_node *rb_prev(struct rb_node node*);
+struct rb_node *rb_first(struct rb_node *root);
+struct rb_node *rb_last(struct rb_node *root);
 
 void rb_replace_node(struct rb_node *oldnode, struct rb_node *newnode,
                      struct rb_root *root);
 
-static inline void rb_link_node(struct rb_node *node,
-                                struct rb_node *parent, struct rb_node **link)
+static inline void rb_link_node(struct rb_node *node, struct rb_node *parent, 
+                                struct rb_node **link)
 {
     node->rb_parent = parent;
     node->rb_left = NULL;
     node->rb_right = NULL;
     node->rb_color = RB_RED;
-
-    link[0] = node;
+    *link = node;
 }
 #endif  //rbtree.h
